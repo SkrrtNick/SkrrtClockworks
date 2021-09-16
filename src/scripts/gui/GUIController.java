9a5a3412aeff75f7e53c814304872682174c6d19
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-09-16 16:02:25 +0100

    Updated controller

--- src/scripts/gui/GUIController.java
+++ src/scripts/gui/GUIController.java
@@ -1,9 +1,61 @@
 package scripts.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GUIController extends AbstractGUIController{
+
+    @FXML
+    private Button btnLoad;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private CheckBox checkButler;
+
+    @FXML
+    private CheckBox checkClockworks;
+
+    @FXML
+    private CheckBox checkMouseSpeed;
+
+    @FXML
+    private CheckBox checkReactions;
+
+    @FXML
+    private CheckBox checkRestocking;
+
+    @FXML
+    private CheckBox checkSkills;
+
+    @FXML
+    private ComboBox<?> comboPricing;
+
+    @FXML
+    private ComboBox<?> comboTeleport;
+
+    @FXML
+    private Slider sliderMouseSpeed;
+
+    @FXML
+    private Slider sliderReactions;
+
+    @FXML
+    private Spinner<?> spinConstruction;
+
+    @FXML
+    private Spinner<?> spinCrafting;
+
+    @FXML
+    private TextField txtProfile;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
