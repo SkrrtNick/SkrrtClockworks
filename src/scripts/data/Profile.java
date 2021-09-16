9a88542a2eef256c47160b7c67db3b71844801ac
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-09-16 17:18:21 +0100

    Added profile saving

---
+++ src/scripts/data/Profile.java
@@ -0,0 +1,10 @@
+package scripts.data;
+
+import lombok.Data;
+@Data
+public class Profile {
+
+    private boolean useButler, restocking, sellClockworks, trainSkills, useCustomMouseSpeed, useCustomReactionTimes;
+    private int craftingGoal, constructionGoal, mouseSpeed, ReactionTimes;
+
+}
--- src/scripts/gui/GUIController.java
+++ src/scripts/gui/GUIController.java
@@ -2,9 +2,12 @@ package scripts.gui;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import org.tribot.script.sdk.util.ScriptSettings;
+import scripts.data.Profile;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -67,9 +70,23 @@ public class GUIController extends AbstractGUIController {
 
     int mouseSpeed, reactions;
 
+    @FXML
+    void btnLoadPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSavePressed(ActionEvent event) {
+        save();
+    }
+
+    @FXML
+    void btnStartPressed(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         sliderMouseSpeed.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
@@ -77,6 +94,31 @@ public class GUIController extends AbstractGUIController {
                 txtMouseSpeed.setText(Integer.toString(mouseSpeed));
             }
         });
+        sliderReactions.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                reactions = (int) sliderReactions.getValue();
+                txtReactionTimes.setText(Integer.toString(reactions));
+            }
+        });
 
     }
+
+    public void save() {
+        ScriptSettings saver = ScriptSettings.getDefault();
+        Profile profile = new Profile();
+        profile.setCraftingGoal(spinCrafting.getValue());
+        profile.setConstructionGoal(spinConstruction.getValue());
+        profile.setUseButler(checkButler.isSelected());
+        profile.setReactionTimes((int) sliderReactions.getValue());
+        profile.setMouseSpeed((int) sliderMouseSpeed.getValue());
+        profile.setRestocking(checkRestocking.isSelected());
+        profile.setSellClockworks(checkClockworks.isSelected());
+        profile.setTrainSkills(checkSkills.isSelected());
+        profile.setUseCustomMouseSpeed(checkMouseSpeed.isSelected());
+        profile.setUseCustomReactionTimes(checkReactions.isSelected());
+        saver.save(txtProfile.getText(), saver);
+    }
+
+
 }
