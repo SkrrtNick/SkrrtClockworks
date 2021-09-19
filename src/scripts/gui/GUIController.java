122e49665418b107c853d0b8bb61668ebee12b31
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-09-19 16:39:06 +0100

    Added Do Not Rename annotations

--- src/scripts/gui/GUIController.java
+++ src/scripts/gui/GUIController.java
@@ -1,5 +1,6 @@
 package scripts.gui;
 
+import com.allatori.annotations.DoNotRename;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -7,68 +8,84 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import org.tribot.script.sdk.Waiting;
 import org.tribot.script.sdk.util.ScriptSettings;
+import scripts.SkrrtClockWork;
 import scripts.api.functions.Loggable;
 import scripts.api.functions.Logger;
+import scripts.data.BuyingOptions;
+import scripts.data.GETeleports;
 import scripts.data.Profile;
+import scripts.data.SellingOptions;
 
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class GUIController extends AbstractGUIController {
-
+    @DoNotRename
     @FXML
     private Button btnLoad;
 
-    @FXML
+    @DoNotRename
+   @FXML
     private Button btnSave;
-
+    @DoNotRename
     @FXML
     private Button btnStart;
-
+    @DoNotRename
     @FXML
     private CheckBox checkButler;
-
+    @DoNotRename
     @FXML
     private CheckBox checkClockworks;
-
+    @DoNotRename
     @FXML
     private CheckBox checkMouseSpeed;
-
+    @DoNotRename
     @FXML
     private CheckBox checkReactions;
-
+    @DoNotRename
     @FXML
     private CheckBox checkRestocking;
-
+    @DoNotRename
     @FXML
     private CheckBox checkSkills;
-
+    @DoNotRename
     @FXML
-    private ComboBox<String> comboPricing;
-
+    private ComboBox<String> comboSelling;
+    @DoNotRename
+    @FXML
+    private ComboBox<String> comboBuying;
+    @DoNotRename
     @FXML
     private ComboBox<String> comboTeleport;
 
+    @DoNotRename
     @FXML
     private Slider sliderMouseSpeed;
 
+    @DoNotRename
     @FXML
     private Slider sliderReactions;
 
+    @DoNotRename
     @FXML
     private Spinner<Integer> spinConstruction;
 
+    @DoNotRename
     @FXML
     private Spinner<Integer> spinCrafting;
 
+    @DoNotRename
     @FXML
     private Text txtMouseSpeed;
 
+    @DoNotRename
     @FXML
     private ComboBox<String> txtProfile;
 
+    @DoNotRename
     @FXML
     private Text txtReactionTimes;
 
@@ -78,33 +95,48 @@ public class GUIController extends AbstractGUIController {
     private SpinnerValueFactory<Integer> constructionFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(25, 50);
     private Logger logger = new Logger().setHeader("GUI");
 
+    @DoNotRename
     @FXML
     void btnLoadPressed(ActionEvent event) {
-        String profileName = txtProfile.getValue();
-        if(profileName.equals("")){
-            logger.setLoggable(Loggable.ERROR)
+        String profileName = txtProfile.getEditor().getText();
+        if (profileName == null || profileName.isBlank()) {
+            logger
+                    .setLoggable(Loggable.ERROR)
                     .setMessage("Please enter a profile name")
                     .print();
-        }
-        else {
+        } else {
             load(txtProfile.getValue());
         }
     }
 
+    @DoNotRename
     @FXML
     void btnSavePressed(ActionEvent event) {
         save(txtProfile.getValue());
     }
 
+    @DoNotRename
     @FXML
     void btnStartPressed(ActionEvent event) {
         save("last");
+        getGUI().close();
+        if (Waiting.waitUntil(() -> !getGUI().isOpen())) {
+            logger.setLoggable(Loggable.MESSAGE)
+                    .setMessage("Welcome to SkrrtClockworks, starting script!")
+                    .print();
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        checkSkills.setDisable(true);
+        spinCrafting.setDisable(true);
+        spinConstruction.setDisable(true);
         updateSaveNames();
         load("last");
+        comboTeleport.setItems(FXCollections.observableList(GETeleports.getList()));
+        comboSelling.setItems(FXCollections.observableList(SellingOptions.getList()));
+        comboBuying.setItems(FXCollections.observableList(BuyingOptions.getList()));
         spinCrafting.setValueFactory(craftingFactory);
         spinConstruction.setValueFactory(constructionFactory);
         sliderMouseSpeed.valueProperty().addListener(new ChangeListener<Number>() {
@@ -141,6 +173,9 @@ public class GUIController extends AbstractGUIController {
                     checkRestocking.setSelected(s.isRestocking());
                     checkRestocking.setSelected(s.isRestocking());
                     sliderReactions.setValue(s.getReactionTimes());
+                    comboTeleport.setValue(s.getGeTeleport());
+                    comboSelling.setValue(s.getSellingOption());
+                    comboBuying.setValue(s.getBuyingOption());
                 });
         updateSaveNames();
     }
@@ -158,7 +193,13 @@ public class GUIController extends AbstractGUIController {
         profile.setTrainSkills(checkSkills.isSelected());
         profile.setUseCustomMouseSpeed(checkMouseSpeed.isSelected());
         profile.setUseCustomReactionTimes(checkReactions.isSelected());
+        profile.setGeTeleport(comboTeleport.getValue());
+        profile.setSellingOption(comboSelling.getValue());
+        profile.setBuyingOption(comboBuying.getValue());
         saver.save(name, profile);
+        logger.setLoggable(Loggable.MESSAGE).setMessage("Successfully saved " + name)
+                .print();
+        SkrrtClockWork.setRunningProfile(profile);
         updateSaveNames();
     }
 
@@ -167,4 +208,5 @@ public class GUIController extends AbstractGUIController {
         List<String> profiles = loadableProfiles.getSaveNames();
         txtProfile.setItems(FXCollections.observableList(profiles));
     }
+
 }
